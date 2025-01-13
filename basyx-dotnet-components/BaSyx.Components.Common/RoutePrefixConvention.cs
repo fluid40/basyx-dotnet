6cbed491b6110c89618cf635e37fdee91851dde7
Constantin Ziesche <constantin.ziesche@bosch.com>
2025-01-13 17:28:44 +0100

    Added route prefix convention for routes

---
+++ basyx-dotnet-components/BaSyx.Components.Common/RoutePrefixConvention.cs
@@ -0,0 +1,40 @@
+﻿/*******************************************************************************
+* Copyright (c) 2024 Bosch Rexroth AG
+* Author: Constantin Ziesche (constantin.ziesche@bosch.com)
+*
+* This program and the accompanying materials are made available under the
+* terms of the MIT License which is available at
+* https://github.com/eclipse-basyx/basyx-dotnet/blob/main/LICENSE
+*
+* SPDX-License-Identifier: MIT
+*******************************************************************************/
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace BaSyx.Components.Common
+{
+    public class RoutePrefixConvention : IControllerModelConvention
+    {
+        private readonly AttributeRouteModel _routePrefix;
+
+        public RoutePrefixConvention(IRouteTemplateProvider route)
+        {
+            _routePrefix = new AttributeRouteModel(route);
+        }
+
+        public void Apply(ControllerModel controller)
+        {
+            foreach (var selector in controller.Selectors)
+            {
+                if (selector.AttributeRouteModel != null)
+                {
+                    selector.AttributeRouteModel = AttributeRouteModel.CombineAttributeRouteModel(_routePrefix, selector.AttributeRouteModel);
+                }
+                else
+                {
+                    selector.AttributeRouteModel = _routePrefix;
+                }
+            }
+        }
+    }
+}
